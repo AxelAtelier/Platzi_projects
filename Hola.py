26e9c74bd04df679b22864ecbd2a5dc10ee68cef
Soni <axel.atelier@gmail.com>
2024-01-02 13:37:17 -0600

    Se ha terminado de crear la funcion de palindromo

--- Hola.py
+++ Hola.py
@@ -4,4 +4,17 @@ Este va a ser mi primer proyecto de control de versiones xd
 '''
 
 def palindromo(string): 
-    pass
+    start = 0
+    end = -1
+    control = len(string)// 2 +1
+    val = True
+
+    while control != 0 and string[start] == string[end]: 
+        control -= 1
+    
+    if control != 0: val = False
+
+    return val
+
+palabra = "AwA"
+print(palindromo(palabra))
