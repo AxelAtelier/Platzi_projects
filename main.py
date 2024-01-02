a2ae606f320c314239776fd65fbaaccafa208758
Soni <axel.atelier@gmail.com>
2024-01-02 14:10:05 -0600

    Se creo el archivo main y definio el metodo palindromo

---
+++ main.py
@@ -0,0 +1,11 @@
+import palindromo as pl 
+
+
+
+def run(): 
+    palabra = "AwA"
+    print(pl.palindromo(palabra))
+
+if __name__ == '__main__': 
+    run()
+
--- palindromo.py
+++ palindromo.py
@@ -16,5 +16,3 @@ def palindromo(string):
 
     return val
 
-palabra = "AwA"
-print(palindromo(palabra))
